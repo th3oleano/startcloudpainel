00da33cf8808a28a64c0253beb71640ff0207145
th3oleano <leo_oleano@hotmail.com>
2026-04-11 10:40:43 -0300

    painel2

--- resources/views/login/login.blade.php
+++ resources/views/login/login.blade.php
@@ -4,7 +4,7 @@
 	<meta charset="UTF-8">
 	<meta name="viewport" content="width=device-width, initial-scale=1.0">
 	<title>Login</title>
-	@vite(['resources/css/app.css', 'resources/css/login.css'])
+	@vite(['resources/css/app.css'])
 </head>
 <body>
 	<div class="login-container">
